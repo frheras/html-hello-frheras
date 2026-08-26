32d05a83525df4f4f44677e1894922d9f83df266
Félix <132064111+frheras@users.noreply.github.com>
2026-08-26 16:20:17 +0000

    ejercicio_hellocopilot

---
+++ helloCopilot.js
@@ -0,0 +1,8 @@
+// Imprime un mensaje de saludo en la consola
+console.log("¡Hola, Copilot!");
+
+function sumar(primerNumero, segundoNumero) {
+	return primerNumero + segundoNumero;
+}
+
+console.log(sumar(2, 3));
